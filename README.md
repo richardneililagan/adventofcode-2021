012df2c5c6f9ef6111bcb260f1fce44a98e8efe1
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2021-12-01 07:44:53 +0000

    feat: solves D1P2

--- README.md
+++ README.md
@@ -6,6 +6,21 @@ This is a collection of my personal solutions for [Advent of Code 2021][aoc2021]
 
 For this year, I'm using AoC2021 as an outlet for learning [Rust][rust].
 
+## Benchmarks
+
+```shell
+$ cargo aoc bench
+```
+
+Solutions and benchmarks are run on a `t6g.medium` [Graviton2][graviton2] instance on AWS EC2.
+
+| | | Part 1 | Part 2 |
+| --- | :-- | :-- | :-- |
+| `01` | **Sonar Sweep** | `4.0161 μs` | `9.6716 μs` |
+
+
+---
+
 ## Thanks!
 
 The project template uses the recommended setup as described by [`cargo-aoc`][cargo-aoc].
@@ -14,6 +29,7 @@ The project template uses the recommended setup as described by [`cargo-aoc`][ca
 
 [Website][website] &middot; [@techlifemusic][twitter]
 
+[graviton2]: https://aws.amazon.com/ec2/graviton
 [aoc2021]: https://adventofcode.com/2021
 [rust]: https://rust-lang.org
 [cargo-aoc]: https://github.com/gobanos/cargo-aoc
--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -25,6 +25,27 @@ pub fn solver_part1(input: &Vec<i32>) -> u32 {
         .sum()
 }
 
+#[aoc(day1, part2)]
+pub fn solver_part2(input: &Vec<i32>) -> u32 {
+    let sums: Vec<i32> = (0..(input.len() - 2))
+        .map(|i| input[i] + input[i + 1] + input[i + 2])
+        .collect();
+
+    sums.iter()
+        .enumerate()
+        .map(|(index, _)| match index {
+            0 => 0,
+            x => {
+                if sums[x] > sums[x - 1] {
+                    1 as u32
+                } else {
+                    0 as u32
+                }
+            }
+        })
+        .sum()
+}
+
 // :: ---
 
 #[cfg(test)]
@@ -45,5 +66,21 @@ mod tests {
                      263";
 
         assert_eq!(solver_part1(&generator(input)), 7);
+    }
+
+    #[test]
+    fn example_2() {
+        let input = "199
+                     200
+                     208
+                     210
+                     200
+                     207
+                     240
+                     269
+                     260
+                     263";
+
+        assert_eq!(solver_part2(&generator(input)), 5);
     }
 }
