f4b40dbc3eae600d50d4b6923489b266c63ee2c1
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2021-12-02 07:23:07 +0000

    feat: adds solution for D2P2

--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -1,6 +1,14 @@
+/// Position for part 1
 struct Position {
   horizon: i32,
   depth: i32,
+}
+
+/// Position for part 2
+struct Position2 {
+  horizon: i32,
+  depth: i32,
+  aim: i32,
 }
 
 pub enum Instruction {
@@ -56,6 +64,35 @@ pub fn solver_part1(input: &Vec<Instruction>) -> i32 {
   end.horizon * end.depth
 }
 
+#[aoc(day2, part2, original)]
+pub fn solver_part2(input: &Vec<Instruction>) -> i32 {
+  let initial = Position2 {
+    horizon: 0,
+    depth: 0,
+    aim: 0,
+  };
+
+  let end = input
+    .iter()
+    .fold(initial, |acc, instruction| match instruction {
+      Instruction::Up(n) => Position2 {
+        aim: acc.aim - n,
+        ..acc
+      },
+      Instruction::Down(n) => Position2 {
+        aim: acc.aim + n,
+        ..acc
+      },
+      Instruction::Forward(n) => Position2 {
+        horizon: acc.horizon + n,
+        depth: acc.depth + (acc.aim * n),
+        ..acc
+      },
+    });
+
+  end.horizon * end.depth
+}
+
 // :: ---
 
 #[cfg(test)]
@@ -72,5 +109,17 @@ mod tests {
                  forward 2";
 
     assert_eq!(solver_part1(&generator(input)), 150);
+  }
+
+  #[test]
+  fn example_2() {
+    let input = "forward 5
+                 down 5
+                 forward 8
+                 up 3
+                 down 8
+                 forward 2";
+
+    assert_eq!(solver_part2(&generator(input)), 900);
   }
 }
