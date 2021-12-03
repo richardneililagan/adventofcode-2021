e6a9d5acb5f2af7b0957d952550f1a456d30193c
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2021-12-03 02:22:42 +0000

    refactor: applies recommended changes by clippy

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -3,12 +3,12 @@ pub fn generator(input: &str) -> Vec<i32> {
     input
         .lines()
         .map(|line| line.trim())
-        .map(|line| i32::from_str_radix(line.trim(), 10).unwrap_or(0))
+        .map(|line| line.parse::<i32>().unwrap())
         .collect()
 }
 
 #[aoc(day1, part1, original)]
-pub fn solver_part1(input: &Vec<i32>) -> u32 {
+pub fn solver_part1(input: &[i32]) -> u32 {
     input
         .iter()
         .enumerate()
@@ -16,9 +16,9 @@ pub fn solver_part1(input: &Vec<i32>) -> u32 {
             0 => 0,
             x => {
                 if input[x] > input[x - 1] {
-                    1 as u32
+                    1
                 } else {
-                    0 as u32
+                    0
                 }
             }
         })
@@ -26,14 +26,14 @@ pub fn solver_part1(input: &Vec<i32>) -> u32 {
 }
 
 #[aoc(day1, part1, windows_fold)]
-pub fn solver_part1_fold(input: &Vec<i32>) -> u32 {
+pub fn solver_part1_fold(input: &[i32]) -> u32 {
     input
         .windows(2)
         .fold(0, |acc, x| if x[1] > x[0] { acc + 1 } else { acc })
 }
 
 #[aoc(day1, part2, original)]
-pub fn solver_part2(input: &Vec<i32>) -> u32 {
+pub fn solver_part2(input: &[i32]) -> u32 {
     let sums: Vec<i32> = (0..(input.len() - 2))
         .map(|i| input[i] + input[i + 1] + input[i + 2])
         .collect();
@@ -44,9 +44,9 @@ pub fn solver_part2(input: &Vec<i32>) -> u32 {
             0 => 0,
             x => {
                 if sums[x] > sums[x - 1] {
-                    1 as u32
+                    1
                 } else {
-                    0 as u32
+                    0
                 }
             }
         })
@@ -54,7 +54,7 @@ pub fn solver_part2(input: &Vec<i32>) -> u32 {
 }
 
 #[aoc(day1, part2, windows_fold)]
-pub fn solver_part2_fold(input: &Vec<i32>) -> u32 {
+pub fn solver_part2_fold(input: &[i32]) -> u32 {
     input
         .windows(3)
         .map(|w| w[0] + w[1] + w[2])
@@ -77,7 +77,7 @@ pub fn solver_part2_fold(input: &Vec<i32>) -> u32 {
 /// since the sliding window will always have the same middle 2 numbers,
 /// (and therefore does not affect the sum comparison).
 #[aoc(day1, part2, windows_fold_edges)]
-pub fn solver_part2_edges(input: &Vec<i32>) -> u32 {
+pub fn solver_part2_edges(input: &[i32]) -> u32 {
     input
         .windows(4)
         .fold(0, |acc, w| if w[3] > w[0] { acc + 1 } else { acc })
--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -1,125 +1,125 @@
 /// Position for part 1
 struct Position {
-  horizon: i32,
-  depth: i32,
+    horizon: i32,
+    depth: i32,
 }
 
 /// Position for part 2
 struct Position2 {
-  horizon: i32,
-  depth: i32,
-  aim: i32,
+    horizon: i32,
+    depth: i32,
+    aim: i32,
 }
 
 pub enum Instruction {
-  Forward(i32),
-  Up(i32),
-  Down(i32),
+    Forward(i32),
+    Up(i32),
+    Down(i32),
 }
 
 #[aoc_generator(day2)]
 pub fn generator(input: &str) -> Vec<Instruction> {
-  input
-    .lines()
-    .map(|line| line.trim().split(' '))
-    .map(|mut split| {
-      (
-        split.next().unwrap(),
-        split.next().unwrap().parse::<i32>().unwrap(),
-      )
-    })
-    .map(|pair| match pair {
-      ("forward", n) => Instruction::Forward(n),
-      ("up", n) => Instruction::Up(n),
-      ("down", n) => Instruction::Down(n),
-      _ => unreachable!(),
-    })
-    .collect()
+    input
+        .lines()
+        .map(|line| line.trim().split(' '))
+        .map(|mut split| {
+            (
+                split.next().unwrap(),
+                split.next().unwrap().parse::<i32>().unwrap(),
+            )
+        })
+        .map(|pair| match pair {
+            ("forward", n) => Instruction::Forward(n),
+            ("up", n) => Instruction::Up(n),
+            ("down", n) => Instruction::Down(n),
+            _ => unreachable!(),
+        })
+        .collect()
 }
 
 #[aoc(day2, part1, original)]
-pub fn solver_part1(input: &Vec<Instruction>) -> i32 {
-  let initial = Position {
-    horizon: 0,
-    depth: 0,
-  };
+pub fn solver_part1(input: &[Instruction]) -> i32 {
+    let initial = Position {
+        horizon: 0,
+        depth: 0,
+    };
 
-  let end = input
-    .iter()
-    .fold(initial, |acc, instruction| match instruction {
-      Instruction::Forward(n) => Position {
-        horizon: acc.horizon + n,
-        ..acc
-      },
-      Instruction::Up(n) => Position {
-        depth: acc.depth - n,
-        ..acc
-      },
-      Instruction::Down(n) => Position {
-        depth: acc.depth + n,
-        ..acc
-      },
-    });
+    let end = input
+        .iter()
+        .fold(initial, |acc, instruction| match instruction {
+            Instruction::Forward(n) => Position {
+                horizon: acc.horizon + n,
+                ..acc
+            },
+            Instruction::Up(n) => Position {
+                depth: acc.depth - n,
+                ..acc
+            },
+            Instruction::Down(n) => Position {
+                depth: acc.depth + n,
+                ..acc
+            },
+        });
 
-  end.horizon * end.depth
+    end.horizon * end.depth
 }
 
 #[aoc(day2, part2, original)]
-pub fn solver_part2(input: &Vec<Instruction>) -> i32 {
-  let initial = Position2 {
-    horizon: 0,
-    depth: 0,
-    aim: 0,
-  };
+pub fn solver_part2(input: &[Instruction]) -> i32 {
+    let initial = Position2 {
+        horizon: 0,
+        depth: 0,
+        aim: 0,
+    };
 
-  let end = input
-    .iter()
-    .fold(initial, |acc, instruction| match instruction {
-      Instruction::Up(n) => Position2 {
-        aim: acc.aim - n,
-        ..acc
-      },
-      Instruction::Down(n) => Position2 {
-        aim: acc.aim + n,
-        ..acc
-      },
-      Instruction::Forward(n) => Position2 {
-        horizon: acc.horizon + n,
-        depth: acc.depth + (acc.aim * n),
-        ..acc
-      },
-    });
+    let end = input
+        .iter()
+        .fold(initial, |acc, instruction| match instruction {
+            Instruction::Up(n) => Position2 {
+                aim: acc.aim - n,
+                ..acc
+            },
+            Instruction::Down(n) => Position2 {
+                aim: acc.aim + n,
+                ..acc
+            },
+            Instruction::Forward(n) => Position2 {
+                horizon: acc.horizon + n,
+                depth: acc.depth + (acc.aim * n),
+                ..acc
+            },
+        });
 
-  end.horizon * end.depth
+    end.horizon * end.depth
 }
 
 // :: ---
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn example_1() {
-    let input = "forward 5
+    #[test]
+    fn example_1() {
+        let input = "forward 5
                  down 5
                  forward 8
                  up 3
                  down 8
                  forward 2";
 
-    assert_eq!(solver_part1(&generator(input)), 150);
-  }
+        assert_eq!(solver_part1(&generator(input)), 150);
+    }
 
-  #[test]
-  fn example_2() {
-    let input = "forward 5
+    #[test]
+    fn example_2() {
+        let input = "forward 5
                  down 5
                  forward 8
                  up 3
                  down 8
                  forward 2";
 
-    assert_eq!(solver_part2(&generator(input)), 900);
-  }
+        assert_eq!(solver_part2(&generator(input)), 900);
+    }
 }
