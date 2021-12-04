990cac0fed25c122a3184051b8ea7157aedbe946
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2021-12-04 08:26:41 +0000

    feat: adds solution for D4P2

--- src/solutions/day4.rs
+++ src/solutions/day4.rs
@@ -88,7 +88,23 @@ pub fn solver_part1((called_nums, b): &(Vec<i32>, Vec<Board>)) -> i32 {
         };
     }
 
-    1
+    unreachable!();
+}
+
+#[aoc(day4, part2, original)]
+pub fn solver_part2((called_nums, b): &(Vec<i32>, Vec<Board>)) -> i32 {
+    let mut boards = b.clone();
+
+    for &n in called_nums {
+        boards.iter_mut().for_each(|b| b.call_number(n));
+        if boards.len() > 1 {
+            boards.retain(|b| !b.is_bingo());
+        } else if boards[0].is_bingo() {
+            return boards[0].uncalled_value() * n;
+        }
+    }
+
+    unreachable!();
 }
 
 // :: ---
@@ -120,5 +136,30 @@ mod tests {
                          2  0 12  3  7";
 
         assert_eq!(solver_part1(&generator(input)), 4512);
+    }
+
+    #[test]
+    fn example_2() {
+        let input: &str = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+                        22 13 17 11  0
+                         8  2 23  4 24
+                        21  9 14 16  7
+                         6 10  3 18  5
+                         1 12 20 15 19
+      
+                         3 15  0  2 22
+                         9 18 13 17  5
+                        19  8  7 25 23
+                        20 11 10 24  4
+                        14 21 16 12  6
+      
+                        14 21 17 24  4
+                        10 16 15  9 19
+                        18  8 23 26 20
+                        22 11 13  6  5
+                         2  0 12  3  7";
+
+        assert_eq!(solver_part2(&generator(input)), 1924);
     }
 }
