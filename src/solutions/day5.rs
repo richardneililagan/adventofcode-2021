b8455883ce0596ab63c842267b83cb9ce8a94689
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2021-12-05 09:17:14 +0000

    feat: adds solution for D5P2

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -8,6 +8,13 @@ struct Point(i32, i32);
 pub struct Segment(Point, Point);
 
 impl Segment {
+    fn is_diagonal(&self) -> bool {
+        let p1 = self.0;
+        let p2 = self.1;
+
+        (p1.0 != p2.0) && (p1.1 != p2.1)
+    }
+
     /// Get the points that are on this line segment, including the endpoints.
     fn get_points(&self) -> Option<Vec<Point>> {
         match self {
@@ -33,7 +40,24 @@ impl Segment {
 
                 Some(points)
             }
-            _ => None,
+            Segment(Point(x1, y1), Point(x2, y2)) => {
+                let xrange = if x1 < x2 {
+                    (*x1..=*x2).collect::<Vec<i32>>()
+                } else {
+                    (*x2..=*x1).rev().collect::<Vec<i32>>()
+                };
+
+                let yrange = if y1 < y2 {
+                    (*y1..=*y2).collect::<Vec<i32>>()
+                } else {
+                    (*y2..=*y1).rev().collect::<Vec<i32>>()
+                };
+
+                let pairwise = xrange.iter().zip(yrange.into_iter());
+                let points = pairwise.map(|(&x, y)| Point(x, y)).collect();
+
+                Some(points)
+            }
         }
     }
 }
@@ -64,14 +88,35 @@ pub fn generator(input: &str) -> Vec<Segment> {
 pub fn solver_part1(input: &[Segment]) -> usize {
     let mut map: HashMap<Point, i32> = HashMap::new();
 
+    input
+        .iter()
+        .filter(|segment| !segment.is_diagonal())
+        .for_each(|segment| {
+            let result_points = segment.get_points();
+            match result_points {
+                None => (),
+                Some(points) => points.iter().for_each(|p| {
+                    let map_point = map.entry(*p).or_insert(0);
+                    *map_point += 1;
+                }),
+            }
+        });
+
+    map.iter().filter(|(_, &i)| i >= 2).count()
+}
+
+#[aoc(day5, part2, original)]
+pub fn solver_part2(input: &[Segment]) -> usize {
+    let mut map: HashMap<Point, i32> = HashMap::new();
+
     input.iter().for_each(|segment| {
         let result_points = segment.get_points();
         match result_points {
-            None => (),
             Some(points) => points.iter().for_each(|p| {
                 let map_point = map.entry(*p).or_insert(0);
                 *map_point += 1;
             }),
+            _ => unreachable!(),
         }
     });
 
@@ -98,5 +143,21 @@ mod tests {
                            5,5 -> 8,2";
 
         assert_eq!(solver_part1(&generator(input)), 5);
+    }
+
+    #[test]
+    fn example_2() {
+        let input: &str = "0,9 -> 5,9
+                           8,0 -> 0,8
+                           9,4 -> 3,4
+                           2,2 -> 2,1
+                           7,0 -> 7,4
+                           6,4 -> 2,0
+                           0,9 -> 2,9
+                           3,4 -> 1,4
+                           0,0 -> 8,8
+                           5,5 -> 8,2";
+
+        assert_eq!(solver_part2(&generator(input)), 12);
     }
 }
