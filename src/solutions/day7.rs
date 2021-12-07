7c38f9fc957c5039320047cd61aed43e65f5137f
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2021-12-07 05:26:28 +0000

    feat: adds solution for D7P2

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -11,10 +11,36 @@ pub fn solver_part1(input: &[i32]) -> i32 {
     let cmin = input.iter().min().unwrap();
     let cmax = input.iter().max().unwrap();
 
-    let mut minimumfuel = cmax * input.len() as i32;
+    let mut minimumfuel = i32::MAX;
 
     for i in *cmin..*cmax {
         let s: i32 = input.iter().map(|n| (i - n).abs()).sum();
+        if s < minimumfuel {
+            minimumfuel = s;
+        }
+    }
+
+    minimumfuel
+}
+
+#[aoc(day7, part2, original)]
+pub fn solver_part2(input: &[i32]) -> u128 {
+    let cmin = input.iter().min().unwrap();
+    let cmax = input.iter().max().unwrap();
+
+    let mut minimumfuel = u128::MAX;
+
+    for i in *cmin..*cmax {
+        let s: u128 = input
+            .iter()
+            .map(|n| {
+                let steps: u128 = (i - n).abs() as u128;
+                let total_fuel: u128 = (1..=steps).sum();
+
+                total_fuel
+            })
+            .sum();
+
         if s < minimumfuel {
             minimumfuel = s;
         }
@@ -34,5 +60,12 @@ mod tests {
         let input: &str = "16,1,2,0,4,2,7,1,2,14";
 
         assert_eq!(solver_part1(&generator(input)), 37);
+    }
+
+    #[test]
+    fn example_2() {
+        let input: &str = "16,1,2,0,4,2,7,1,2,14";
+
+        assert_eq!(solver_part2(&generator(input)), 168);
     }
 }
